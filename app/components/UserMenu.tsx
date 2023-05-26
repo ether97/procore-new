10fbb6f07c384204dc4b57e7dbf644435df3d269
Mateusz Zukowki <109141931+ether97@users.noreply.github.com>
2023-05-26 16:15:56 -0400

    added routes to user menu

--- app/components/UserMenu.tsx
+++ app/components/UserMenu.tsx
@@ -1,33 +1,38 @@
 "use client";
 
 import { signOut } from "next-auth/react";
-
-export const userMenuItems = [
-  {
-    label: "Profile",
-    onClick: () => {},
-  },
-  {
-    label: "Settings",
-    onclick: () => {},
-  },
-  {
-    label: "Log out",
-    onClick: () => {
-      signOut();
-    },
-  },
-];
+import { useRouter } from "next/navigation";
 
 const UserMenu = () => {
+  const router = useRouter();
+
+  const userMenuItems = [
+    {
+      label: "Profile",
+      onClick: () => {
+        router.push("/profile");
+      },
+    },
+    {
+      label: "Settings",
+      onclick: () => {},
+    },
+    {
+      label: "Log out",
+      onClick: () => {
+        signOut();
+      },
+    },
+  ];
   return (
-    <div className="absolute bg-black top-[59px] z-100 right-[0px] rounded-bottom-md ">
+    <div className="absolute bg-black top-[59px] z-150 right-[0px] rounded-bottom-md ">
       {userMenuItems.map((item, index) => {
         if (index === 0) {
           return (
             <div
+              key={item.label}
               onClick={item.onClick}
-              className="py-4 px-8 flex flex-row items-center justify-start text-white transition duration-200 hover:bg-yellow-400 hover:text-black cursor-pointer bg-gradient-to-b from-yellow-400"
+              className="z-100 py-4 px-8 flex flex-row items-center justify-start text-white transition duration-200 hover:bg-yellow-400 hover:text-black cursor-pointer bg-gradient-to-b from-yellow-400"
             >
               {item.label}
             </div>
@@ -35,8 +40,9 @@ const UserMenu = () => {
         } else {
           return (
             <div
+              key={item.label}
               onClick={item.onClick}
-              className="py-4 px-8 flex flex-row items-center justify-start text-white transition duration-200 hover:bg-yellow-400 hover:text-black cursor-pointer"
+              className="z-100 py-4 px-8 flex flex-row items-center justify-start text-white transition duration-200 hover:bg-yellow-400 hover:text-black cursor-pointer"
             >
               {item.label}
             </div>

e54a1199feaa4f83869bac6271238087cf83a7d5
Mateusz Zukowki <109141931+ether97@users.noreply.github.com>
2023-05-26 16:16:07 -0400

    changed navabr css

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -13,6 +13,7 @@ import { signOut } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import useMenu from "@/app/hooks/userMenu";
 import UserMenu from "../UserMenu";
+import DisplateSelection from "@/app/pages/displates/DisplateSelection";
 
 interface NavbarProps {
   currentUser?: any | null | undefined;
@@ -22,8 +23,8 @@ const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
   const router = useRouter();
   const menu = useMenu();
   return (
-    <div className="fixed w-full bg-yellow-400 flex flex-row items-center justify-center">
-      <div className="w-full max-w-[1000px] flex flex-row items-center justify-between">
+    <div className="z-100 fixed w-full bg-gradient-to-b from-yellow-400 flex flex-col items-center justify-center">
+      <div className="w-full lg:max-w-[1500px] xl:max-w-[1800px] md:max-w-[1200px] sm:max-w-[1000px] flex flex-row items-center justify-between">
         <div
           className="flex flex-row items-center justify-center gap-3 cursor-pointer"
           onClick={() => {
@@ -36,18 +37,14 @@ const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
 
         <div className="flex flex-row items-center gap-2 py-5">
           <div className="flex flex-col justify-end items-end ">
-            <div className="font-semibold text-black">
-              {currentUser?.user.name}
-            </div>
-            <div className="text-sm text-neutral-500">
-              {currentUser?.user.email}
-            </div>
+            <div className="font-semibold text-black">{currentUser?.name}</div>
+            <div className="text-sm text-neutral-500">{currentUser?.email}</div>
           </div>
-          {currentUser?.user.image ? (
+          {currentUser?.image ? (
             <div className="relative" onClick={() => menu.onToggle()}>
               <Image
                 className="rounded-full cursor-pointer peer"
-                src={currentUser.user.image}
+                src={currentUser?.image}
                 alt="user avatar"
                 width="35"
                 height="35"
@@ -59,6 +56,7 @@ const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
           )}
         </div>
       </div>
+      <DisplateSelection />
     </div>
   );
 };

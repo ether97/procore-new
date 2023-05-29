449aea6f36b2f6080e4117abeb4279dd897c8e73
Mateusz Zukowki <109141931+ether97@users.noreply.github.com>
2023-05-28 20:20:50 -0400

    created category modal

--- app/components/CategoryModal.tsx
+++ app/components/CategoryModal.tsx
@@ -10,12 +10,14 @@ const CategoryModal = () => {
   const displatePerCategory = useSelector(
     (state: RootState) => state.displate.displateCategories
   );
+  const category = useSelector((state: RootState) => state.displate.category);
   return (
     <>
       <input type="checkbox" id="category-modal" className="modal-toggle" />
       <div className="modal p-0">
         <div className="modal-box relative p-[20px]">
-          <h3 className="font-bold text-lg">Title</h3>
+          <h3 className="font-bold text-lg text-center">{category}</h3>
+          <p className="text-sm text-zinc-500 text-center">Collect them all!</p>
           <div className="modal-action absolute top-[0px] right-[30px]">
             <label htmlFor="category-modal">
               <IoMdCloseCircleOutline
@@ -24,16 +26,18 @@ const CategoryModal = () => {
               />
             </label>
           </div>
-          {displatePerCategory.map((displate) => (
-            <Image
-              height={150}
-              width={100}
-              className="rounded-md "
-              alt={displate.category || ""}
-              src={`/images/${displate.category}/${displate.img}.jpg`}
-              style={{ objectFit: "cover" }}
-            />
-          ))}
+          <div className="flex flex-row gap-5 items-center justify-center mt-[20px]">
+            {displatePerCategory.map((displate) => (
+              <Image
+                height={150}
+                width={100}
+                className="rounded-md "
+                alt={displate.category || ""}
+                src={`/images/${displate.category}/${displate.img}.jpg`}
+                style={{ objectFit: "cover" }}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </>

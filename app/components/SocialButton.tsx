fd5a50d6841a61190beef87f93b8bf994c0df273
Mateusz Zukowki <109141931+ether97@users.noreply.github.com>
2023-05-28 20:20:34 -0400

    social button

---
+++ app/components/SocialButton.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { IconType } from "react-icons";
+
+interface SocialButtonProps {
+  label: string;
+  icon?: IconType;
+  onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  color: string;
+}
+
+const SocialButton: React.FC<SocialButtonProps> = ({
+  label,
+  icon: Icon,
+  onClick,
+  color,
+}) => {
+  return (
+    <button
+      className={`bg-gray-700 rounded-full p-2 text-${color} hover:bg-white transition duration-300 ${
+        label !== "Google" ? "hover:text-black" : ""
+      }`}
+      onClick={onClick}
+    >
+      {Icon && <Icon size={24} />}
+    </button>
+  );
+};
+
+export default SocialButton;

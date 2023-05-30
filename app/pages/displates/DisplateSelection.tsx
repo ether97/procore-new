7c4462478f3624e2230d58c776ef9054cf51c66f
Mateusz Zukowki <109141931+ether97@users.noreply.github.com>
2023-05-30 07:46:12 -0400

    modal change

--- app/pages/displates/DisplateSelection.tsx
+++ app/pages/displates/DisplateSelection.tsx
@@ -19,9 +19,8 @@ const DisplateSelection = () => {
     <div className="z-100 w-2/3 shadow-lg flex items-center justify-center p-4">
       <div className="w-full lg:max-w-[1200px] xl:max-w-[1500px] md:max-w-[900px] sm:max-w-[700px] flex items-center justify-between">
         {icons.map((icon) => (
-          <label htmlFor="category-modal">
+          <label htmlFor="category-modal" key={icon}>
             <Image
-              key={icon}
               className="cursor-pointer hover:bg-white rounded-full p-[5px]"
               height="40"
               width="40"

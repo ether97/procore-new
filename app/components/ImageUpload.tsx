021844e2a631588b687c31c0d27916ebaf48d671
Mateusz Zukowki <109141931+ether97@users.noreply.github.com>
2023-05-31 17:10:55 -0400

    image upload

--- app/components/ImageUpload.tsx
+++ app/components/ImageUpload.tsx
@@ -1,8 +1,4 @@
-"use client";
-
-import axios from "axios";
-
-import { toast } from "react-hot-toast";
+import AWS from "aws-sdk";
 
 const ImageUpload = () => {
   const uploadPhoto = async (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -10,27 +6,37 @@ const ImageUpload = () => {
     const filename = file.name;
     const fileType = file.type;
 
-    console.log(file, filename, fileType);
-    const res = await axios(
-      `/api/upload?file=${filename}&fileType=${fileType}`
-    );
-    const { url } = await res.data;
+    console.log("filename", filename, fileType);
 
-    const config = {
-      headers: {
-        "Content-Type": fileType,
-      },
+    var credentials = {
+      accessKeyId: process.env.S3_ACCESS_KEY as string,
+      secretAccessKey: process.env.S3_SECRET_KEY as string,
     };
+    AWS.config.update({ credentials: credentials, region: "us-east-1" });
+    var s3 = new AWS.S3();
 
-    const upload = await axios.put(url, file, config);
-    if (upload.status === 200) {
+    var presignedGETURL = s3.getSignedUrl("getObject", {
+      Bucket: process.env.BUCKET_NAME,
+      Key: filename, //filename
+      Expires: 100, //time to expire in seconds
+    });
+
+    // const res = await fetch(`/api/s3?file=${filename}&fileType=${fileType}`);
+    // const { url } = await res.json();
+
+    console.log(presignedGETURL);
+
+    const upload = await fetch(presignedGETURL, {
+      method: "PUT",
+      body: file,
+      headers: { "Content-Type": fileType },
+    });
+
+    if (upload.ok) {
       console.log("Uploaded successfully!");
     } else {
       console.error("Upload failed.");
     }
-
-    const s3FileUrl = `https://<nextjspractice>.s3.us-east-2.amazonaws.com/${filename}`;
-    console.log("file url: ", s3FileUrl);
   };
 
   return (
